96850be977d78aa2ed9ba71b4a66cfb42039baac
Zain <abibz97@gmail.com>
2025-08-21 21:26:11 +0700

    modified allproduct

--- src/Pages/allproductpage.tsx
+++ src/Pages/allproductpage.tsx
@@ -1,14 +1,14 @@
 import Header from "../components/Fragments/Header";
-import AllProductLayout from "../components/Layouts/AllProductLayout"; 
+import AllProductLayout from "../components/Layouts/AllProductLayout";
 import Footer from "../components/Fragments/Footer";
 
 const AllProduct = () => {
   return (
-    <>
+    <div  className="flex flex-col justify-between h-screen">
       <Header />
       <AllProductLayout />
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/components/Layouts/AllProductLayout.tsx
+++ src/components/Layouts/AllProductLayout.tsx
@@ -1,9 +1,45 @@
 import TitleCollectionVideo from "../Elements/Card/titlecollectionvideo";
+import Arrow from "../../assets/rightArrow.png";
 
 const AllProduct = () => {
   return (
-    <div className="flex flex-col justify-center px-5 md:px-30 py-7 md:py-16 md:gap-16 gap-6 w-full items-center">
+    <div className="flex flex-col px-5 md:px-30 py-7 md:py-16 md:gap-16 gap-6 items-center ">
       <TitleCollectionVideo />
+      <div className="flex gap-10.5">
+        {/* filter container */}
+        <div className="rounded-[10px] bg-white border md:p-5 p-4 flex flex-col gap-4 border-[#3A35411F] w-90">
+          <div className="flex justify-between items-center">
+            <p className="font-semibold text-[18px] leading-[120%] font-poppins text-[#333333]">
+              Filter
+            </p>
+            <button className="font-medium text-base leading-[140%] tracking-[0.2px] text-[#FF5C2B] cursor-pointer">
+              Reset
+            </button>
+          </div>
+          <div className="rounded-[10px] bg-white border px-4 py-3 flex flex-col gap-4.5 border-[#3A35411F]">
+            <div className="flex justify-between cursor-pointer hover:text-[#3ECF4C] ">
+              <button className="font-medium text-base leading-[140%] tracking-[0.2px] ">
+                Bidang Studi
+              </button>
+              <img src={Arrow} alt="arrow" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <button className="font-medium text-base leading-[140%] tracking-[0.2px] text-[#333333] cursor-pointer">
+                Pemasaran
+              </button>
+              <button className="font-medium text-base leading-[140%] tracking-[0.2px] text-[#333333] cursor-pointer">
+                Digital & Teknologi
+              </button>
+              <button className="font-medium text-base leading-[140%] tracking-[0.2px] text-[#333333] cursor-pointer">
+                Pengembangan Diri
+              </button>
+              <button className="font-medium text-base leading-[140%] tracking-[0.2px] text-[#333333] cursor-pointer">
+                Bisnis Manajemen
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
